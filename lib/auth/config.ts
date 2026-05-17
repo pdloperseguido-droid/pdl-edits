83f19992b404c46f40237d295a044cf7184a09f1
pdl <pdloperseguido@gmail.com>
2026-05-17 02:08:32 -0300

    update

--- lib/auth/config.ts
+++ lib/auth/config.ts
@@ -26,6 +26,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
       profile(profile) {
         return {
           id: profile.sub,
